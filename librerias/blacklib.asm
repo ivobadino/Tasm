bd82429f284f97395a2988d53e892fa655b9b8c1
ivobadino <148398896+ivobadino@users.noreply.github.com>
2025-06-06 20:25:49 -0300

    agrego carpeta con llibrerias

---
+++ librerias/blacklib.asm
@@ -0,0 +1,25 @@
+.8086
+.model small
+.stack 100h
+.data
+
+.code
+	;╔╦═════╦╦══LISTA DE FUNCIONES══════════╦╦══════════════╦╗
+	;║║		║║								║║				║║
+	;║║ 01. ║║ 		IMPRIMIR TEXTO 		-->>║║	linea 00000	║║
+	 ║║		║║	public imprimirTextof		║║				║║
+	;║║		║║								║║				║║
+	;║║ 02. ║║		CARAGAR CARACTERES 	-->>║║	linea 00000	║║
+	 ║║		║║	public cargaCaracteresf		║║				║║
+	;║║		║║								║║				║║
+	;║║ 03. ║║							-->>║║	linea 00000	║║
+	 ║║		║║	public						║║				║║
+	;║║		║║								║║				║║
+	;╚╩═════╩╩══LISTA DE FUNCIONES══════════╩╩══════════════╩╝
+
+;==========================================================
+;
+	
+
+
+end	
